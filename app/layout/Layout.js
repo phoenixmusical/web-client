db058120f20c474b280f0fe042f8ad6912ef0ab3
Frédéric Gascon <frederic.gascon@gmail.com>
2016-10-27 00:58:54 -0400

    don't use classes in layout

--- app/layout/Layout.js
+++ app/layout/Layout.js
@@ -5,6 +5,21 @@ import Header from './Header';
 import Navigation from './Navigation';
 import style from './Layout.css';
 
+const styles = {
+    container: {
+        position: 'relative',
+    },
+    content: {
+        marginTop: 50,
+        marginLeft: 0,
+        padding: '10px 20px',
+        transition: 'all 450ms cubic-bezier(0.23, 1, 0.32, 1) 0ms',
+    },
+    contentOpen: {
+        marginLeft: 256,
+    },
+};
+
 class Layout extends Component {
     constructor (props) {
         super(props);
@@ -22,15 +37,20 @@ class Layout extends Component {
     render () {
         const { app, children } = this.props;
         const { navigationOpen } = this.state;
+        const contentStyle = Object.assign({}, styles.content);
+        if (navigationOpen) {
+            Object.assign(contentStyle, styles.contentOpen);
+        }
+
         return (
             <div>
                 <Header
                     onToggleNavigation={() => this.toggleNavigation()} />
-                <div className={style.container}>
+                <div style={styles.container}>
                     <Navigation
                         app={app}
                         open={navigationOpen} />
-                    <div className={classNames(style.content, { [style.open]: navigationOpen })}>
+                    <div style={contentStyle}>
                         {children}
                     </div>
                 </div>
