0a24dd17256a3f7db15136f291cc1faf545a2e30
Frédéric Gascon <frederic.gascon@gmail.com>
2016-11-12 10:41:39 -0500

    update calendar

--- app/routes/home/HomePage.js
+++ app/routes/home/HomePage.js
@@ -13,8 +13,11 @@ const styles = {
     lengendLabel: {
         marginRight: 10,
         fontWeight: 'bold',
+        display: 'inline-block',
     },
 };
+
+const calendarURL = '';
 
 class HomePage extends Component {
     render () {
@@ -24,17 +27,50 @@ class HomePage extends Component {
                 <h1>Phoenix Musical</h1>
                 <h4>Calendrier</h4>
                 <div>
-                    <span style={styles.lengendLabel}>
-                        <span style={styles.colorBlock('#f83a22')} /> Chant
-                    </span>
-                    <span style={styles.lengendLabel}>
-                        <span style={styles.colorBlock('#16a765')} /> Chorégraphie
-                    </span>
-                    <span style={styles.lengendLabel}>
-                        <span style={styles.colorBlock('#4986e7')} /> Mise en scène
-                    </span>
+                    {[
+                        {
+                            label: 'Chant',
+                            color: 'rgb(173, 45, 45)',
+                        },
+                        {
+                            label: 'Chorégraphie',
+                            color: 'rgb(60, 153, 91)',
+                        },
+                        {
+                            label: 'Mise en scène',
+                            color: '#4986e7',
+                        },
+                        {
+                            label: 'Comité Accessoires',
+                            color: 'rgb(230, 115, 153)',
+                        },
+                        {
+                            label: 'Comité Costumes',
+                            color: 'rgb(224, 194, 64)',
+                        },
+                        {
+                            label: 'Comité Décors',
+                            color: 'rgb(167, 184, 40)',
+                        },
+                        {
+                            label: 'Comité Maquillage/Coiffure',
+                            color: 'rgb(96, 63, 153)',
+                        },
+                        {
+                            label: 'Comité Marketing',
+                            color: 'rgb(126, 194, 37)',
+                        },
+                        {
+                            label: 'Comité Social',
+                            color: 'rgb(199, 86, 30)',
+                        },
+                    ].map((item, index) => (
+                        <span key={index} style={styles.lengendLabel}>
+                            <span style={styles.colorBlock(item.color)} /> {item.label}
+                        </span>
+                    ))}
                 </div>
-                <iframe src="https://calendar.google.com/calendar/embed?showTitle=0&amp;mode=WEEK&amp;height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=phoenix.musical%40outlook.com&amp;color=%231B887A&amp;src=avdnq1961piusnokel3f5fq8nk%40group.calendar.google.com&amp;color=%23711616&amp;src=dbnrj0l10vs8v1lnvedv2sgn3s%40group.calendar.google.com&amp;color=%23125A12&amp;src=cop471rdbb3atvc54lvhts1un0%40group.calendar.google.com&amp;color=%23182C57&amp;ctz=America%2FToronto" style={{ borderWidth: 0 }} width="800" height="600" frameBorder="0" scrolling="no"></iframe>
+                <iframe src="https://calendar.google.com/calendar/embed?showTitle=0&amp;mode=WEEK&amp;height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=phoenix.musical%40outlook.com&amp;color=%231B887A&amp;src=avdnq1961piusnokel3f5fq8nk%40group.calendar.google.com&amp;color=%23711616&amp;src=dbnrj0l10vs8v1lnvedv2sgn3s%40group.calendar.google.com&amp;color=%23125A12&amp;src=s11osvmsi83v2i178c6pqqreps%40group.calendar.google.com&amp;color=%23B1365F&amp;src=10nmjrfph9ldc412bujagm8gbc%40group.calendar.google.com&amp;color=%23AB8B00&amp;src=k5jspkogt531i127pspb3303vk%40group.calendar.google.com&amp;color=%235F6B02&amp;src=r83i1rb2t9dqqbn11mis4n6ans%40group.calendar.google.com&amp;color=%2323164E&amp;src=v8c77k9kpojdelabhumccslapg%40group.calendar.google.com&amp;color=%232F6309&amp;src=2s30njikqv0djbm7u3g0tjkqes%40group.calendar.google.com&amp;color=%23853104&amp;src=cop471rdbb3atvc54lvhts1un0%40group.calendar.google.com&amp;color=%23182C57&amp;ctz=America%2FToronto" style={{ borderWidth: 0 }} width="800" height="600" frameBorder="0" scrolling="no"></iframe>
             </div>
         );
     }
